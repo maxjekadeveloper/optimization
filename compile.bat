f14a8dc0aef352162051e20e7b78847009aeb0ce
Max Jeka <max.software.engineer.com>
2026-04-28 20:44:01 +0300

    added compile.bat

---
+++ compile.bat
@@ -0,0 +1,5 @@
+g++ --std=c++20 -O3 optimize.cpp -o optimize
+g++ --std=c++20 -O3 optimize2.cpp -o optimize2
+g++ --std=c++20 -O3 optimize3.cpp -o optimize3
+g++ --std=c++20 -O3 optimize4.cpp -o optimize4
+g++ --std=c++20 -O3 all.cpp -o all
